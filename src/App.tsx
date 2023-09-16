6776c04f0b5a2891ee4e6c42115e8a85a9100790
Mhmdamin <mohamadaminseyedi00@gmail.com>
2023-09-17 01:33:28 +0330

    config is Done:)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-[100vh] overflow-hidden">
+      <div className=" text-3xl text-white bg-violet-600 hover:bg-violet-800 py-4 px-5 rounded-md">Quera-Task-Manager</div>
+    </div>
+  );
+}
+
+export default App;
