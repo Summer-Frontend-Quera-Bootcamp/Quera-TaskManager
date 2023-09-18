b903d9fac98c9af0a56cd6d919166e7d10644aef
saeed <szare295@gmAIL.COM>
2023-09-18 12:40:28 +0330

    test-ul for tailwind

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+const index = () => {
+    return (
+    <header className="">
+        <ul className="flex flex-row-reverse">
+            <li className = "ml-auto">کوئرا تسک منیجر</li>
+            <li>ثبت‌نام نکرده‌ای؟</li>
+            <li>ثبت‌نام</li>
+        </ul>
+
+    </header>
+    )
+  }
+  
+  export default index
+  
