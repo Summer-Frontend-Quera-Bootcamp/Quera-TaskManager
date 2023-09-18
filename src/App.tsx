a8941e194fce59d49ba03658d9ffd5320888fc77
Mhmdamin <mohamadaminseyedi00@gmail.com>
2023-09-19 02:40:19 +0330

    Idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 // import Navbar from './components/Navbar'
-// import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar'
 // import Header from './components/Header'
-import Login from './components/Login'
+// import Login from './components/Login'
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Login/>
-        
+      {/* <Login/> */}
+      <Sidebar/>
     </div>
   );
 }
