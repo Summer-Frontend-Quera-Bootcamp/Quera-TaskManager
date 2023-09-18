cd442b1ea2f0fa29ba3b4c74591062da39b06673
saeed <szare295@gmAIL.COM>
2023-09-18 16:29:54 +0330

    completed the Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 const index = () => {
     return (
         <header className = "m-[80px]">
-            <ul className="flex flex-row-reverse">
+            <ul className="flex flex-row-reverse items-center">
                 <li className="ml-auto bg-clip-text bg-gradient-to-r text-HL from-[#118C80] to-[#4AB7D8] font-extra-bold text-transparent">کوئرا تسک منیجر </li>
                 <li>ثبت‌نام نکرده‌ای؟</li>
-                <li><button className="bg-[#208D8E]">ثبت‌نام</button></li>
+                <li><button className=" bg-[#208D8E]  mr-2 w-[95px] text-TS  font-extra-bold text-[#FFFFFF]  px-[26px] py-[8px] rounded-md  hover:bg-teal-700">ثبت‌نام</button></li>
             </ul>
         </header>
     )
