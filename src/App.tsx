b5028a41da7d89023d6d238826a3006473c555e2
saeed <szare295@gmAIL.COM>
2023-09-18 13:10:27 +0330

    learning & testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
+// import Navbar from './components/Navbar'
+// import Sidebar from './components/Sidebar'
+import Header from './components/Header'
 import './App.css';
 
 function App() {
   return (
     <div className="flex justify-between overflow-hidden">
-      <Navbar/>
-      <Sidebar/>
+      {/* <Navbar/> */}
+      {/* <Sidebar/> */}
+      <Header/>
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,12 @@
 const index = () => {
     return (
-    <header className="">
-        <ul className="flex flex-row-reverse">
-            <li className = "ml-auto">کوئرا تسک منیجر</li>
-            <li>ثبت‌نام نکرده‌ای؟</li>
-            <li>ثبت‌نام</li>
-        </ul>
-
-    </header>
+        <header>
+            <ul className="flex flex-row-reverse">
+                <li className="ml-auto">کوئرا تسک منیجر </li>
+                <li>ثبت‌نام نکرده‌ای؟</li>
+                <li>ثبت‌نام</li>
+            </ul>
+        </header>
     )
   }
   
