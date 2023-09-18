7e9ef2bb7447ea52ad78feb040ca6c972a8bb3f6
juniorsForever <im.not.ali@outlook.com>
2023-09-19 03:15:01 +0330

    Fixed:SearchBoxIcon

--- src/components/Searchbox/index.tsx
+++ src/components/Searchbox/index.tsx
@@ -1,9 +1,13 @@
+import {FaSearch} from 'react-icons/fa'
 const index = () => {
   return (
     <div className="flex justify-end mt-[16px]">
+      <div className="flex items-center">
+        <FaSearch className="text-[#AAAAAA] text-TS"/>
         <input className="w-full text-right outline-none bg-[#F6F7F9] placeholder-[#AAAAAA] placeholder:text-TS" type="text" placeholder="جستوجو  کنید"/>
+      </div>
     </div>
-  )
+    )
 }
 
 export default index
