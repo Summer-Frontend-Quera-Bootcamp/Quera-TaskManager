1a93c01db95ab6ab7cdf41cd5321d60ff5339272
Mhmdamin <mohamadaminseyedi00@gmail.com>
2023-09-20 11:29:40 +0330

    ADD: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 // import Header from './components/Header'
-// import Login from './components/Login'
+import Login from './components/Login'
 // import Form from './components/Form'
-// import SignUp from './components/SignUp'
+import SignUp from './components/SignUp'
 
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="grid grid-cols-main">
-      {/* <Login/> */}
-      {/* <Form/> */}
-      {/* <SignUp/> */}
-      <Navbar/>
-      <Sidebar/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/SignUp" element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom'
 const index = () => {
     return (
         <header className = "m-[80px] mb-[0px]">
             <ul className="flex flex-row-reverse items-center">
                 <li className="ml-auto bg-clip-text bg-gradient-to-r text-HL from-[#118C80] to-[#4AB7D8] font-extra-bold text-transparent">کوئرا تسک منیجر </li>
                 <li>ثبت‌نام نکرده‌ای؟</li>
-                <li><button className=" bg-[#208D8E]  mr-2 w-[95px] text-TS  font-extra-bold text-[#FFFFFF]  px-[26px] py-[8px] rounded-md  hover:bg-teal-700">ثبت‌نام</button></li>
+                <li><Link to="/SignUp"><button className=" bg-[#208D8E]  mr-2 w-[95px] text-TS  font-extra-bold text-[#FFFFFF]  px-[26px] py-[8px] rounded-md  hover:bg-teal-700">ثبت‌نام</button></Link></li>
             </ul>
         </header>
     )
