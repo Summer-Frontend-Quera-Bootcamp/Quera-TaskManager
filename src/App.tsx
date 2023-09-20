51eb4f34d8868660f237c7cf6694c589e75f2b15
Code-57 <Sina.shahvali021@gmail.com>
2023-09-20 09:35:00 +0330

    some changes in Profile1 components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 // import Navbar from './components/Navbar'
 // import Sidebar from './components/Sidebar'
 // import Header from './components/Header'
-import Login from './components/Login'
+// import Login from './components/Login'
 // import Form from './components/Form'
 // import SignUp from './components/SignUp'
+import Profile1 from './components/Profile1'
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Login/>
+      {/* <Login/> */}
       {/* <Form/> */}
         {/* <SignUp/> */}
+        <Profile1/>
     </div>
   );
 }
---
+++ src/components/Profile1/index.tsx
@@ -0,0 +1,21 @@
+
+const Index = () => {
+    return (
+        <>
+        <div className="box-content h-[55px] w-[235px] float-right top-[40px] left-[55px] text-right leading-[55.27px] font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-[#118C80] to-[#4AB7D8] mr-[50px] mt-[40px] ">کوئرا تسک منیجر</div>
+
+
+        </>
+    )
+
+}
+
+
+
+
+
+
+
+
+
+export default Index
