128bf91b6fc52158cd64aafc9b147b1a07daa1fa
Mhmdamin <mohamadaminseyedi00@gmail.com>
2023-09-22 11:04:39 +0330

    ADD: routing for forgot pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import SignUp from './components/SignUp'
 import Main from './Pages/Main/Index'
 import NotFound from './Pages/NotFound/Index'
 import Profile1 from './components/Profile1/index'
+import Forgot from './Pages/Forgot'
+import Linksend from './Pages/Linksend'
+import Forgotnewpass from './Pages/Forgotnewpass'
 import './App.css';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
@@ -15,6 +18,9 @@ function App() {
         <Route path="/SignUp" element={<SignUp/>} />
         <Route path="/main" element={<Main/>} />
         <Route path="/404" element={<NotFound/>} />
+        <Route path="/forgot" element={<Forgot/>} />
+        <Route path="/linksend" element={<Linksend/>} />
+        <Route path="/Forgotnewpass" element={<Forgotnewpass/>} />
         <Route path="*" element={<Navigate to="404"/>} />
         <Route path="/Profile1" element={<Profile1/>} />
       </Routes>
