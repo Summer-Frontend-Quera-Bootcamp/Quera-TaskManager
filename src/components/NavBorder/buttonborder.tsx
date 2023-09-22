d87b6b1d813d5ba1025829282df58a3aca2a4a8c
Ali <im.not.ali@outlook.com>
2023-09-22 22:51:00 +0330

    Add => [Set all Animations of Navbar]

--- src/components/NavBorder/buttonborder.tsx
+++ src/components/NavBorder/buttonborder.tsx
@@ -1,15 +1,24 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 
 interface BorderProps {
   width: number;
-  right:number;
+  right: number;
 }
 
-const NavBorder: React.FC<BorderProps> = ({ width,right}) => {
+const NavBorder: React.FC<BorderProps> = ({ width, right }) => {
+  const [showBorder, setShowBorder] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setShowBorder(true), 100);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div
-      className="h-[0px] origin-top rotate-180 border-[#208D8E] border-2 absolute"
-      style={{ width: `${width}px` , right: `${right}px`}}
+      className={`h-0 origin-top rotate-180 border-[#208D8E] border-2 absolute transition-all duration-500 ${
+        showBorder ? "opacity-100" : "opacity-0"
+      }`}
+      style={{ width: `${width}px`, right: `${right}px` }}
     ></div>
   );
 };
