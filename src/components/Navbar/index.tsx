c9d1db1446ac4bfcdec2a9d62a20a153b83a31fd
Ali <im.not.ali@outlook.com>
2023-09-23 04:53:00 +0330

    Add => [Complete Navbar With Animations  and Elements (without celender elements)]

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,59 +1,135 @@
-import { useState } from "react"
+import { useState } from "react";
 import SvgCol from "../iconComponents/col";
 import SvgCelender from "../iconComponents/celender";
 import SvgDoList from "../iconComponents/dolist";
-import Shareicon from "../iconComponents/Shareicon"
+import Shareicon from "../iconComponents/Shareicon";
 import Navborder from "../NavBorder/buttonborder";
+import NavBoxSearch from "../NavbarSearch/navBoxSearch";
+import SvgFilter from "../iconComponents/filter";
+
 const Index = () => {
   const [clickedIndex, setClickedIndex] = useState<number | null>(null);
-  const handleClick = (index: number) => clickedIndex === index ? null : setClickedIndex(index);
-  const setStyle = (index: number) => (clickedIndex === index ? { color: "#208D8E", fontWeight: "bold"} : {});
+  const handleClick = (index: number) =>
+    clickedIndex === index ? null : setClickedIndex(index);
+  const setStyle = (index: number) =>
+    clickedIndex === index ? { color: "#208D8E", fontWeight: "bold" } : {};
   return (
-    <div className=" h-[125px] border-b-2 fixed right-[356px] top-[41px] left-[50px]">
+    <div className=" h-[125px] fixed right-[356px] top-[41px] left-[50px]">
       <div className="text-right border-b-2 h-[64px] flex items-center justify-between">
         <div className="flex cursor-pointer">
           <p className="font-bold text-TM">اشتراک‌گذاری</p>
-          <Shareicon/>
+          <Shareicon />
         </div>
         <div className="flex flex-row-reverse items-center space-x-3">
           <h1 className="ml-3 text-HXS font-extra-bold">پروژه اول</h1>
-          <div><svg width="1" height="22" viewBox="0 0 1 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="0.5" y1="2.18556e-08" x2="0.499999" y2="22" stroke="#999999"/>
+          <div>
+            <svg
+              width="1"
+              height="22"
+              viewBox="0 0 1 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                x1="0.5"
+                y1="2.18556e-08"
+                x2="0.499999"
+                y2="22"
+                stroke="#999999"
+              />
             </svg>
           </div>
           <div className="flex cursor-pointer">
-          <p className="mr-1 font-extrabold select-none text-TM" style={setStyle(0)} onClick={()=>handleClick(0)}>نمایش لیستی</p>
-            
-          <div style={setStyle(0)}>
-            <SvgDoList/>
+            <p
+              className="mr-1 font-extrabold select-none text-TM"
+              style={setStyle(0)}
+              onClick={() => handleClick(0)}
+            >
+              نمایش لیستی
+            </p>
+
+            <div style={setStyle(0)}>
+              <SvgDoList />
+            </div>
           </div>
-          </div>
-          <div><svg width="1" height="22" viewBox="0 0 1 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="0.5" y1="2.18556e-08" x2="0.499999" y2="22" stroke="#999999"/>
+          <div>
+            <svg
+              width="1"
+              height="22"
+              viewBox="0 0 1 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                x1="0.5"
+                y1="2.18556e-08"
+                x2="0.499999"
+                y2="22"
+                stroke="#999999"
+              />
             </svg>
           </div>
           <div className="flex cursor-pointer">
-          <p className="mr-1 font-extrabold select-none text-TM" style={setStyle(1)} onClick={()=>handleClick(1)}>نمایش ستونی</p>
-          <div style={setStyle(1)}>
-            <SvgCol />
+            <p
+              className="mr-1 font-extrabold select-none text-TM"
+              style={setStyle(1)}
+              onClick={() => handleClick(1)}
+            >
+              نمایش ستونی
+            </p>
+            <div style={setStyle(1)}>
+              <SvgCol />
+            </div>
           </div>
-          </div>
-          <div><svg width="1" height="22" viewBox="0 0 1 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="0.5" y1="2.18556e-08" x2="0.499999" y2="22" stroke="#999999"/>
+          <div>
+            <svg
+              width="1"
+              height="22"
+              viewBox="0 0 1 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                x1="0.5"
+                y1="2.18556e-08"
+                x2="0.499999"
+                y2="22"
+                stroke="#999999"
+              />
             </svg>
           </div>
           <div className="flex cursor-pointer">
-          <p className="mr-1 font-extrabold select-none text-TM" style={setStyle(2)} onClick={()=>handleClick(2)}>تقویم</p>
-          <div style={setStyle(2)}>
-          <SvgCelender/>
+            <p
+              className="mr-1 font-extrabold select-none text-TM"
+              style={setStyle(2)}
+              onClick={() => handleClick(2)}
+            >
+              تقویم
+            </p>
+            <div style={setStyle(2)}>
+              <SvgCelender />
+            </div>
           </div>
-          </div>
-          <div><svg width="1" height="22" viewBox="0 0 1 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="0.5" y1="2.18556e-08" x2="0.499999" y2="22" stroke="#999999"/>
+          <div>
+            <svg
+              width="1"
+              height="22"
+              viewBox="0 0 1 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                x1="0.5"
+                y1="2.18556e-08"
+                x2="0.499999"
+                y2="22"
+                stroke="#999999"
+              />
             </svg>
           </div>
         </div>
       </div>
+
       {clickedIndex !== null && (
         <div onClick={() => handleClick(clickedIndex)}>
           <Navborder
@@ -62,8 +138,80 @@ const Index = () => {
           />
         </div>
       )}
+      <div
+        className={`transition-transform ${
+          clickedIndex !== null
+            ? "transform translate-y-0 opacity-100"
+            : "transform           -translate-y-full opacity-0"
+        } ease-in-out duration-300`}
+      >
+        {clickedIndex === 2 ? (
+          <div className="flex flex-row-reverse ">
+            <NavBoxSearch />
+            <div className="mt-[19.5px] mb-[19.px] mr-[236px]">
+              <svg
+                width="1"
+                height="22"
+                viewBox="0 0 1 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <line
+                  x1="0.5"
+                  y1="2.18556e-08"
+                  x2="0.499999"
+                  y2="22"
+                  stroke="#999999"
+                />
+              </svg>
+            </div>
+          </div>
+        ) : (
+          <div
+            className={`transition-transform ${
+              clickedIndex !== null
+                ? "transform translate-y-0 opacity-100"
+                : "transform -translate-y-full opacity-0"
+            } ease-in-out duration-300`}
+          >
+            {clickedIndex !== null && (
+              <div className="flex flex-row-reverse ">
+                <NavBoxSearch />
+                <div className="mt-[19.5px] mb-[19.px] mr-[236px]">
+                  <svg
+                    width="1"
+                    height="22"
+                    viewBox="0 0 1 22"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <line
+                      x1="0.5"
+                      y1="2.18556e-08"
+                      x2="0.499999"
+                      y2="22"
+                      stroke="#999999"
+                    />
+                  </svg>
+                </div>
+                <div className="flex flex-row cursor-pointer">
+                  <p className="mr-[6px] text-stone-900 text-xs font-normal mb-[20px] mt-[22px]">
+                    فیلترها
+                  </p>
+                  <SvgFilter />
+                </div>
+                <div className="w-[155px] h-[29px] px-3 py-1 bg-blue-100 rounded justify-center items-center inline-flex mt-[16px] mr-[16px]">
+                  <div className="text-right text-blue-500 text-xs font-normal  capitalize">
+                    دسته‌بندی‌شده با: وضعیت
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
---
+++ src/components/iconComponents/filter.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+function SvgFilter (props:React.SVGProps<SVGSVGElement>) {
+    return(
+
+        <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-[40px] mt-[20px] mb-[20px]" >
+        <path d="M6.12132 2.37868C7.29289 3.55025 7.29289 5.44975 6.12132 6.62132C4.94975 7.79289 3.05025 7.79289 1.87868 6.62132C0.707107 5.44975 0.707107 3.55025 1.87868 2.37868C3.05025 1.20711 4.94975 1.20711 6.12132 2.37868" stroke="#323232" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M18 4.5H7" stroke="#323232" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M18.1213 12.3787C19.2929 13.5502 19.2929 15.4497 18.1213 16.6213C16.9497 17.7929 15.0502 17.7929 13.8787 16.6213C12.7071 15.4497 12.7071 13.5502 13.8787 12.3787C15.0502 11.2071 16.9497 11.2071 18.1213 12.3787" stroke="#323232" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M2 14.5H13" stroke="#323232" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    )
+}
+
+export default SvgFilter;
