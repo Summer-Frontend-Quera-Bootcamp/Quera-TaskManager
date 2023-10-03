6ce5b300877503e3b3f97425728074376ad27120
zhra vanaki <zhra.vnk78@gmail.com>
2023-10-03 13:22:38 -0700

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@
 // import Main from './Pages/Main/Index'
 // import NotFound from './Pages/NotFound/Index'
 // import Profile1 from './components/Profile1/index'
-import Profile3 from './components/Profile3/index'
+import Profile3 from "./components/Profile3/index";
 // import Forgot from './Pages/Forgot'
 // import Linksend from './Pages/Linksend'
 // import Forgotnewpass from './Pages/Forgotnewpass'
-import './App.css';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import ShowInformation from './components/ShowInformation/Index';
-
 
 function App() {
   return (
@@ -25,7 +24,7 @@ function App() {
         <Route path="/Forgotnewpass" element={<Forgotnewpass/>} />
         <Route path="*" element={<Navigate to="404"/>} /> */}
         {/* <Route path="/Profile1" element={<Profile1/>} /> */}
-        <Route path="/Profile3" element={<Profile3/>} />
+        <Route path="/Profile3" element={<Profile3 />} />
         {/* <Route path="/ShowInformation" element={<ShowInformation/>} /> */}
       </Routes>
     </BrowserRouter>
--- src/components/Profile3/index.tsx
+++ src/components/Profile3/index.tsx
@@ -1,9 +1,9 @@
-import Darkmode from "../iconComponents/Darkmodeicon";
-import SvgMoon from "../iconComponents/moon";
 import Sidebar from "../Sidebar";
 import React, { useState } from "react";
+import Darkmode from "../iconComponents/Darkmodeicon";
+import SvgMoon from "../iconComponents/moon";
 
-const Index = () => {
+  const Index2 = () => {
   const [backgroundColor, setBackgroundColor] = useState("white");
   const handleButtonClick = (color: React.SetStateAction<string>) => {
     setBackgroundColor(color);
@@ -14,7 +14,6 @@ const Index = () => {
     setMoonActive(!isMoonActive);
     setIoToLeft(!isMoonActive);
   };
-
 
   return (
     <div
@@ -31,7 +30,6 @@ const Index = () => {
         <p className="text-right text-[#1E1E1E] text-[31px] font-IRANYekan font-bold break-words">
           تنظیمات
         </p>
-
         <div className="flex flex-col justify-start items-end gap-6">
           <div className="h-[16px] flex flex-col justify-start items-end gap-2">
             <div className="text-right text-[#1E1E1E] text-base font-IRANYekan font-normal capitalize break-words">
@@ -41,131 +39,127 @@ const Index = () => {
               <div className="justify-center items-center gap-[13.36px] inline-flex">
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#FD7E14")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#FD7E14] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none "></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#FD7E14] rounded-[8px] border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
-                  <button
-                    onClick={() => handleButtonClick("#FAB005")}
-                    className="   border: ? '1px solid blue' : '1px solid black'"
-                  >
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#FAB005]  border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                  <button onClick={() => handleButtonClick("#FAB005")}>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#FAB005] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#82C91E")}>
                     {" "}
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#82C91E] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none" ></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#82C91E] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#40C057")}>
                     {" "}
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#40C057] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#40C057] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
-
                 <div className="w-[20px] h-[20px] relative">
-                  <button onClick={() => handleButtonClick("208D8E")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#208D8E] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                  <button onClick={() => handleButtonClick("#208D8E")}>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#208D8E] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("white")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-white border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-white rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
 
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#12B886")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#12B886] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#12B886] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#15AABF")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#15AABF] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#15AABF] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#228BE6")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#228BE6] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#228BE6] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#4C6EF5")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#4C6EF5] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#4C6EF5] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#7950F2")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#7950F2] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#7950F2] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
-                  <button onClick={() => handleButtonClick("#BE4BDB")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#BE4BDB] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                  <button onClick={() => handleButtonClick("#BE4BDB")} >
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#BE4BDB] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#E64980")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#E64980] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#E64980] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
                 <div className="w-[20px] h-[20px] relative">
                   <button onClick={() => handleButtonClick("#FA5252")}>
-                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#FA5252] border-2 rounded-[8px] focus:border-blue-500  focus:outline-none"></div>
+                    <div className="w-[20px] h-[20px] left-[0px] top-[0px] absolute bg-[#FA5252] rounded-[8px]  border-2 border-b-purple-200"></div>
                   </button>
                 </div>
               </div>
             </div>
-            <div className="w-[24px] h-[24px] relative flex flex-col justify-start items-start mr-12 ">
-              <div className="relative">
-                <div
-                  className={`cursor-pointer ml-[8px] h-[36px] w-[64px] flex flex-row items-center rounded-md ${
-                    isMoonActive
-                      ? "bg-[#111827] transition-all duration-500"
-                      : "bg-gray-100 transition-all duration-500"
-                  }`}
-                  onClick={toggleMoon}
-                >
+
+            <button>
+              <div className="w-[24px] h-[24px] relative flex flex-col justify-start items-start m-4 mr-12">
+                <div className="w-[24px] h-[24px] ralative">
                   <div
-                    className={`w-[30px] h-[30px] absolute top-[3px] rounded-md ${
-                      isIoToLeft
-                        ? "bg-[#6b7280] transition-all duration-500"
-                        : "bg-white transition-all duration-500"
+                    className={`cursor-pointer ml-[8px] h-[36px] w-[64px] flex flex-row items-center rounded-md ${
+                      isMoonActive
+                        ? "bg-[#111827] transition-all duration-500"
+                        : "bg-gray-100 transition-all duration-500"
                     }`}
-                    style={{ left: isIoToLeft ? "11px" : "40px" }}
-                    id="io"
-                  ></div>
-                  <div
-                    style={{
-                      position: "relative",
-                      zIndex: 1,
-                      transition: "opacity 0.5s ease-in-out",
-                      opacity: isMoonActive ? 1 : 0,
-                    }}
+                    onClick={toggleMoon}
                   >
-                    <button onClick={() => handleButtonClick("white")}>
+                    <div
+                      className={`w-[30px] h-[30px] absolute top-[3px] rounded-md ${
+                        isIoToLeft
+                          ? "bg-[#6b7280] transition-all duration-500"
+                          : "bg-white transition-all duration-500"
+                      }`}
+                      style={{ left: isIoToLeft ? "11px" : "40px" }}
+                      id="io"
+                    ></div>
+                    <div
+                      style={{
+                        position: "relative",
+                        zIndex: 1,
+                        transition: "opacity 0.5s ease-in-out",
+                        opacity: isMoonActive ? 1 : 0,
+                      }}
+                    >
                       <SvgMoon />
-                    </button>
-                  </div>
-                  <div
-                    style={{
-                      position: "relative",
-                      zIndex: 1,
-                      transition: "opacity 0.5s ease-in-out",
-                      opacity: isMoonActive ? 0 : 1,
-                    }}
-                  >
-                    <button onClick={() => handleButtonClick("#0f172a")}>
+                    </div>
+                    <div
+                      style={{
+                        position: "relative",
+                        zIndex: 1,
+                        transition: "opacity 0.5s ease-in-out",
+                        opacity: isMoonActive ? 0 : 1,
+                      }}
+                    >
                       <Darkmode />
-                    </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </button>
+
             <button>
-              <div className="w-[354px] h-auto px-4 py-2 bg-teal-500 rounded-md flex justify-center items-center gap-2 mt-4">
+              <div className="w-[354px] h-auto px-4 py-2 bg-teal-500 rounded-md flex justify-center items-center gap-2 ">
                 <div className="text-right text-white text-lg font-bold font-iranyekan capitalize break-words">
                   تغییرات
                 </div>
@@ -177,4 +171,5 @@ const Index = () => {
     </div>
   );
 };
-export default Index;
+
+export default Index2;
