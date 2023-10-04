0b988af8ad6c08436c3b4de548058f382342f08d
mhmdAmin <mohamadaminseyedi00@gmail.com>
2023-10-04 10:56:45 +0330

    FIX: calendar (add persian ...)

--- src/Pages/Main/Index.tsx
+++ src/Pages/Main/Index.tsx
@@ -1,14 +1,14 @@
 import Sidebar from '../../components/Sidebar';
 import Navbar from '../../components/Navbar'
-// import Calendar from '../../components/Calendar/Index'
-import Columndisplay from '../../components/Columndisplay'
+import Calendar from '../../components/Calendar/Index'
+// import Columndisplay from '../../components/Columndisplay'
 const Index = () => {
   return (
     <div className="overflow-hidden">
       <Navbar/>
       <Sidebar/>
-      {/* <Calendar/> */}
-      <Columndisplay/>
+      <Calendar/>
+      {/* <Columndisplay/> */}
     </div>
   )
 }
